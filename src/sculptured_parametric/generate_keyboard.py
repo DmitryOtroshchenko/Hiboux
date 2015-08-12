dc84c22883d8b5d4e7498be51691c27f12740dcd
Dmitry Otroshchenko <dmitry.otroshchenko@gmail.com>
2015-08-12 15:15:34 +0200

    Move todos to github.

--- src/sculptured_parametric/generate_keyboard.py
+++ src/sculptured_parametric/generate_keyboard.py
@@ -178,14 +178,6 @@ def main():
     a15 = a90 / 6
     a10 = a90 / 9
 
-    # TODO: add column widths. 2
-    # TODO: add y offsets between columns. 1
-    # TODO: Add row widths. 3
-    # TODO: Add spacers. 3
-    # TODO: Add thumb clusters. 5
-    # TODO: Add different keycap models. 5
-    # TODO: Figure out why column height offsets work. 2
-    # TODO: Improve colors. 1
     masks = [
         [True, True, True, True, True],
         [True, True, True, True, True],
